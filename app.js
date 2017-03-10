71f0c74ff0686b9dcbdc58917690aa74b60eb5ff
semigradsky <semigradskyd@gmail.com>
2017-03-10 22:20:23 +0300

    Add result view

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const rp = require('request-promise');
 const cheerio = require('cheerio');
+const exphbs = require('express-handlebars');
 
 const app = express();
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -20,13 +24,16 @@ const parsePage = name => html => {
 	const $ = cheerio.load(html);
 
 	const parseInfo = (_, el) => {
+		const state = $(el).find('td:nth-child(6)').text();
 		return {
 			regNumber: $(el).find('td:nth-child(2)').text(),
 			name: $(el).find('td:nth-child(3)').text(),
 			regAuthority: $(el).find('td:nth-child(4)').text(),
 			reqDate: $(el).find('td:nth-child(5)').text(),
-			state: $(el).find('td:nth-child(6)').text(),
-			expDate: $(el).find('td:nth-child(7)').text()
+			state,
+			expDate: $(el).find('td:nth-child(7)').text(),
+			expired: state.indexOf('Исключен из ЕГР') >= 0,
+			valid: state.indexOf('Действующий') >= 0
 		};
 	};
 
@@ -47,8 +54,7 @@ app.post('/search', (req, res) => {
 	Promise.all(
 		names.map(name => rp('http://egr.gov.by/egrn/index.jsp?content=Find&fmax=1000&vname=' + encodeURIComponent(name)).then(parsePage(name)))
 	).then(data => {
-		res.setHeader('Content-Type', 'text/plain');
-		res.end(JSON.stringify(data, null, 4));
+		res.render('search', { results: data, layout: false });
 	}).catch(err => {
 		console.error(err);
 	});
